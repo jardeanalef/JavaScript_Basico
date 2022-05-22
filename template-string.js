19528698f7bc9c0ecbed318becc1ce683db52017
jardean <jardeanalef@gmail.com>
2022-05-22 18:57:11 -0300

    adicionando nova funcionalidade de comandos JavaScript - arquivo template-string.js

---
+++ template-string.js
@@ -0,0 +1,13 @@
+let meuNome = 'Jardean';
+let meuSobrenome = 'Liarte';
+let minhaProfissao = 'Front-End Developer';
+
+//Sem Template String
+console.log('Olá, eu sou ' + meuNome + ' ' + meuSobrenome + ' minha Profissão é: ' + minhaProfissao);
+
+
+console.log(`Olá, eu sou ${meuNome} ${meuSobrenome} minha profissão é: ${minhaProfissao}`);
+
+console.log(`O resultado da soma 1 + 1 = ${1 + 1}`);
+
+console.log(`O objeto json ${{ chave: 'valor'}}`);
